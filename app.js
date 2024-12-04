e2300b8c95891b5516777629a8c9e84a6e658b8b
rothasok <rothasok@gmail.com>
2024-12-04 15:19:39 +0700

    V8

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello World 12345');
+    res.status(200).send('Hello World 123456');
 });
 
 app.get('/user/:id', (req, res) => {
--- app.test.js
+++ app.test.js
@@ -7,7 +7,7 @@ describe('GET /', () => {
     it('should return Hello, World!', async () => {
         const res = await request(app).get('/');
         expect(res.statusCode).toEqual(200);
-        expect(res.text).toBe('Hello World 1234');
+        expect(res.text).toBe('Hello World 123456');
     });
 });
 
